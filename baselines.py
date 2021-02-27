76c66708d133ab67fddfcb93a1bd37838bef0e72
C. Kevin Chen <ckchen95@stanford.edu>
2021-02-27 11:41:15 -0800

    Add debug statements

--- baselines.py
+++ baselines.py
@@ -43,7 +43,9 @@ def main():
     tweets, _, labels = load_csv(csv_path)
 
     bert_base = load_bert_base()
+    print('BERT BASE loaded.')
     preds = classify_sentiment(bert_base, tweets)
+    print('Inference complete.')
     acc = compute_accuracy(preds, labels)
     print('BERT BASE Accuracy:', acc)
 
